f53ef0fadf4511e45581a5f73fc9c8a6fc447980
Mel Phillips <melphillips@Mels-MacBook-Pro-2.local>
2022-07-08 12:54:00 -0400

    adding a spreadsheet on retirees by department, and latest queries on joining tables.

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -16,9 +16,128 @@ FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
--- Create a new table for the retirment info.
-SELECT first_name, last_name
+-- Create retirement_info table from the employees table.
+SELECT emp_no, first_name, last_name
 INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Joining departments and dept_manager tables
+SELECT d.dept_name,
+	dm.emp_no,
+	dm.from_date,
+	dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+-- Joining retirment_info and dept_emp tables
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
+
+-- Creating current_emp table from retirment_info and dept emp tables
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO retires_by_dept
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+
+-- Create retirement_info table from the employees table.
+SELECT e.emp_no, 
+	e.first_name, 
+	e.last_name, 
+	e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	AND (de.to_date = '9999-01-01');
+
+-- List of managers per dept.
+SELECT dm.dept_no,
+	d.dept_name,
+	dm.emp_no,
+	ce.last_name,
+	ce.first_name,
+	dm.from_date,
+	dm.to_date
+INTO manager_info
+FROM dept_manager as dm
+INNER JOIN departments as d
+ON (dm.dept_no = d.dept_no)
+INNER JOIN current_emp as ce
+ON (dm.emp_no = ce.emp_no);
+
+-- Department retirees.
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp as de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments as d
+ON (de.dept_no = d.dept_no);
+
+-- What's going on with the salaries?
+-- Why are there only five active managers for nine departments?
+-- Why are some employees appearing twice on?
+
+SELECT * FROM departments;
+SELECT * FROM retires_by_dept;
+
+-- List of sales retirees.
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+-- INTO sales_info
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON (ce.emp_no = de.emp_no)
+LEFT JOIN departments as d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_name = ('Sales');
+
+-- List of sales and development retirees.
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+-- INTO sales_dev_info
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON (ce.emp_no = de.emp_no)
+LEFT JOIN departments as d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_name IN ('Sales', 'Development');
